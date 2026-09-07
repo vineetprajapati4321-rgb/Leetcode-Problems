fa529bb984f54104adaf0b3332eae5c398a7a4bf
Vineet Prajapati <vineetprajapati4321@gmail.com>
2026-09-08 05:22:47 +0530

    Time: 521 ms (83.42%), Space: 27.6 MB (58.5%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,17 +1,12 @@
 class Solution:
-    def sortArray(self, nums):
-        if len(nums) <= 1:
-            return nums
+    
 
-        mid = len(nums) // 2
-
-        left = self.sortArray(nums[:mid])
-        right = self.sortArray(nums[mid:])
-
+    def merge_array(self, left, right):
         result = []
-        i = j = 0
+        i, j = 0, 0
+        n, m = len(left), len(right)
 
-        while i < len(left) and j < len(right):
+        while i < n and j < m:
             if left[i] <= right[j]:
                 result.append(left[i])
                 i += 1
@@ -19,7 +14,26 @@ class Solution:
                 result.append(right[j])
                 j += 1
 
-        result.extend(left[i:])
-        result.extend(right[j:])
+        while i < n:
+            result.append(left[i])
+            i += 1
+
+        while j < m:
+            result.append(right[j])
+            j += 1
 
         return result
+
+    def merge_sort(self, arr):
+        if len(arr) <= 1:
+            return arr
+
+        mid = len(arr) // 2
+
+        left_arr = self.merge_sort(arr[:mid])
+        right_arr = self.merge_sort(arr[mid:])
+
+        return self.merge_array(left_arr, right_arr)
+
+    def sortArray(self, nums):
+        return self.merge_sort(nums)
